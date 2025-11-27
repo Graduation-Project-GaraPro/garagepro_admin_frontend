21d44204de080008b7b9f2cebf58ce2b0dc9260a
letienks2410 <anhemgameplay@gmail.com>
2025-11-28 00:38:09 +0700

    Logs-> logs

--- garapro-admin-frontend/src/app/admin/monitoringLogs/page.tsx
+++ garapro-admin-frontend/src/app/admin/monitoringLogs/page.tsx
@@ -1,4 +1,4 @@
-import { AdvancedSystemLogs } from '@/components/admin/Logs/AdvancedSystemLogs'
+import { AdvancedSystemLogs } from '@/components/admin/logs/AdvancedSystemLogs'
 export default function LogsPage() {
 
   return <AdvancedSystemLogs />
